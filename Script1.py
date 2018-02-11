c7eaaaab21ad17bcc767f57f15d2956afd3dfa42
Carlos Salinas <csalinas@invlat.com>
2018-02-11 15:27:37 -0600

    Mi primer archio del repositorio

---
+++ Script1.py
@@ -0,0 +1,10 @@
+'''
+Mi primer script
+Certificado en seguridad de la info.
+'''
+
+print("Programa que suma 2 numeros")
+a = int(input("Ingrese el sumando 1:"))
+b = int(input("Ingrese el sumando 2:"))
+suma = a + b #Suma lo que el usuario ingresa
+print("El resultado es:", suma)
